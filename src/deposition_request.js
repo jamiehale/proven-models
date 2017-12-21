619267dadac59e4f263a268e9e41b9c7007072d8
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-12-21 17:48:55 +0100

    revert var/const thing

--- src/deposition_request.js
+++ src/deposition_request.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var DepositionRequestSchema = new Schema({
+const DepositionRequestSchema = new Schema({
     ipfsHash: String,
     remoteAddress: String,
     submittedAt: {type: Date, default: Date.now},
